4e93d790e05e6e3308bea723c4558e14705d9772
sagardwivedi <Sagar.dw20@gmail.com>
2022-06-01 00:38:44 +0530

    Pre: Added custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    colors: {
+      blue: {
+        100: "#2b3945",
+        200: "#202c37",
+        300: "#111517",
+      },
+      gray: {
+        100: "#fafafa",
+        200: "#858585",
+      },
+      black: "#000",
+      white: "#fff",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
