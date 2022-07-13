2ed1fa33e24eed972f375c8e596c56841f40a02a
sagardwivedi <Sagar.dw20@gmail.com>
2022-07-13 15:58:15 +0530

    Functionality Changes

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -27,13 +27,13 @@ const Filter: FC<FilterProps> = ({
         <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className={`filter-icon h-5 w-5 transition duration-300 ease-out`}
+            className="filter-icon h-5 w-5 transition duration-300 ease-in-out"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
             <path
               fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
               clipRule="evenodd"
             />
           </svg>
